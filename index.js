8254aa17f6bcd366c6c8808423385c8ab23c0744
AhmedTLA <ahmedtlaidjia123@gmail.com>
2023-11-08 16:29:14 +0100

    fixed

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
     credentials : true,
-    origin : 'http://localhost:5173',
+    origin : 'http://192.168.1.129:5173',
     allowedHeaders :['Content-Type', 'Authorization']
 }))
  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "http://192.168.1.129:5173");
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
