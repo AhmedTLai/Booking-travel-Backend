2cf6dabc2aac73ed7066b6a6957744ff5937cbd2
AhmedTLA <ahmedtlaidjia123@gmail.com>
2023-11-09 11:09:28 +0100

    updated

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cors({
-    credentials: true,
-}))
+app.use(cors())
 // app.use(function (req, res, next) {
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
